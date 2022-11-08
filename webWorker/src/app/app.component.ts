3a6fb6e16be7eb0c1ee85d907605e88ac8db71dc
tanujarobosoft <tanuja.s@robosoftin.com>
2022-11-08 20:05:08 +0530

    updated

---
+++ webWorker/src/app/app.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { PrimeComponent } from './prime/prime.component';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'webWorker';
+  prime10 : number = 0;
+  prime10000 : number = 0;
+
+  find10thPrimeNumber() {
+     this.prime10 = PrimeComponent.findNthPrimeNumber(10);
+  }
+
+  find10000thPrimeNumber() {
+     if (typeof Worker !== 'undefined') {
+       
+        const worker = new Worker(new URL('./app.worker', import.meta.url));
+        
+        worker.onmessage = ({ data }) => {
+        this.prime10000 = data;
+        };
+        worker.postMessage(10000);
+     } else {
+        // Web Workers are not supported in this environment.
+        // You should add a fallback so that your program still executes correctly.
+     }
+  }
+}
+
+
